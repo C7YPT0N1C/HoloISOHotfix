3a4a5c0b7b31c9ca36475291d123a870b4dfd1ae
C7YPT0N1C <peaceolawuyi01@gmail.com>
2023-01-05 13:45:43 +0000

    removed development files

--- HoloISOHotfix.sh
+++ HoloISOHotfix.sh
@@ -6,27 +6,81 @@
 # holoiso-grub-update
 # steamos-sessions-select [plasma|gamescope|plasma-x11-persistent]
 
+echo "Beginning repository setup."
 
-read -r -p "Do you want to use the '-rel' repositories (1) or the '-3.3' repositories (2)?  (1/2)?" choice # Chooses whether to use -rel repos or -3.3 repos
-if [ "$choice" = "1" ]; then
+read -r -p "Do you want to use the '-rel' repositories or the '-3.3' repositories?  [rel/3.3] (Default = rel): " choice # Chooses whether to use -rel repos or -3.3 repos
+if [ "$choice" = "rel" ]; then
         echo "Selecting '-rel' repositories."
-        sudo mv ./pacman/pacman-rel-stable.conf ./pacman.conf
-elif [ "$choice" = "2" ]; then
+        
+        read -r -p "Do you want to use the stable branch?  [y/n] (Default = y): " choice # Chooses whether to use "holoiso-stable" branch or the "holoiso" branch
+        if [ "$choice" = "y" ]; then
+            echo "Selecting stable branch."
+
+            read -r -p "File '/etc/pacman.conf' will be overwritten. Continue? (y/n): " choice # Update pacman.conf to latest working repos based on selected repositories
+            if [ "$choice" = "y" ]; then
+                echo "Updating file '/etc/pacman.conf'"
+                sudo cp ./pacman/pacman-rel-stable.conf ./pacman.conf
+            elif [ "$choice" = "n" ]; then
+                echo "Skipping updating file '/etc/pacman.conf'"
+            else
+                echo "Invalid Answer."
+                exit
+            fi
+
+        elif [ "$choice" = "n" ]; then
+            echo "Deselecting stable branch."
+
+            read -r -p "File '/etc/pacman.conf' will be overwritten. Continue? (y/n): " choice
+            if [ "$choice" = "y" ]; then
+                echo "Updating file '/etc/pacman.conf'"
+                sudo cp ./pacman/pacman-rel-holoiso.conf ./pacman.conf
+            elif [ "$choice" = "n" ]; then
+                echo "Skipping updating file '/etc/pacman.conf'"
+            else
+                echo "Invalid Answer."
+                exit
+            fi
+        else
+            echo "Invalid Answer."
+            exit
+        fi
+
+elif [ "$choice" = "3.3" ]; then
         echo "Selecting '-3.3' repositories."
-        sudo mv ./pacman/pacman-3.3-stable.conf ./pacman.conf
-else
-        echo "Invalid Answer."
-        exit
-fi
 
+        read -r -p "Do you want to use the stable branch?  [y/n] (Default = y): " choice
+        if [ "$choice" = "y" ]; then
+            echo "Selecting stable branch."
 
+            read -r -p "File '/etc/pacman.conf' will be overwritten. Continue? (y/n): " choice
+            if [ "$choice" = "y" ]; then
+                echo "Updating file '/etc/pacman.conf'"
+                sudo cp ./pacman/pacman-3.3-stable.conf ./pacman.conf
+            elif [ "$choice" = "n" ]; then
+                echo "Skipping updating file '/etc/pacman.conf'"
+            else
+                echo "Invalid Answer."
+                exit
+            fi
 
-read -r -p "File '/etc/pacman.conf' will be overwritten. Continue (y/n)?" choice # Update pacman.conf to latest working repos
-if [ "$choice" = "y" ]; then
-        echo "Updating file '/etc/pacman.conf'"
-        sudo mv ./pacman.conf /etc/pacman.conf
-elif [ "$choice" = "n" ]; then
-        echo "Skipping updating file '/etc/pacman.conf'"
+        elif [ "$choice" = "n" ]; then
+            echo "Deselecting stable branch."
+
+            read -r -p "File '/etc/pacman.conf' will be overwritten. Continue? (y/n): " choice
+            if [ "$choice" = "y" ]; then
+                echo "Updating file '/etc/pacman.conf'"
+                sudo cp ./pacman/pacman-3.3-holoiso.conf ./pacman.conf
+            elif [ "$choice" = "n" ]; then
+                echo "Skipping updating file '/etc/pacman.conf'"
+            else
+                echo "Invalid Answer."
+                exit
+            fi
+            
+        else
+            echo "Invalid Answer."
+            exit
+        fi
 else
         echo "Invalid Answer."
         exit
